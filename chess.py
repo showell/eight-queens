87dd2c59426c72089b83bdb3d40ecf3a3af18200
root <showell30@yahoo.com>
2023-02-15 01:43:06 +0000

    Add sw/ne diagonals.
    It might be a mistake to delve into the trickiest part of
    the N-queens problem so early. Figuring out diagonal attacks
    can be tricky.
    
    But I am not super scared by diagonals.
    
    https://www.askdifference.com/northeast-vs-southwest/

--- chess.py
+++ chess.py
@@ -1,5 +1,12 @@
 # Tested with Python 3.10.7
 
+def sw_ne_diagonal(x, y):
+    return x - y
+
+def sw_ne_diagonal_str(x, y):
+    diag = sw_ne_diagonal(x, y)
+    return f"{diag: 4d}"
+
 def vanilla_square(x, y):
         letter = 'ABCDEFGH'[x]
         return f' {letter}{y+1}({x}, {y})'
@@ -10,4 +17,4 @@ def print_board(square):
             print(square(x, y), end='')
         print()
 
-print_board(vanilla_square)
+print_board(sw_ne_diagonal_str)
