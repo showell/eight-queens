741900296e55a483a1cad6593bb079c7725149f5
root <showell30@yahoo.com>
2023-02-15 01:59:15 +0000

    CODE CLEANUP: Remove some debugging methods.
    https://dennyfamily.files.wordpress.com/2011/07/before-after.jpg

--- chess.py
+++ chess.py
@@ -3,20 +3,8 @@
 def sw_ne_diagonal(x, y):
     return x - y
 
-def sw_ne_diagonal_str(x, y):
-    diag = sw_ne_diagonal(x, y)
-    return f"{diag: 4d}"
-
 def nw_se_diagonal(x, y):
     return x + y
-
-def nw_se_diagonal_str(x, y):
-    diag = nw_se_diagonal(x, y)
-    return f"{diag: 4d}"
-
-def vanilla_square(x, y):
-        letter = 'ABCDEFGH'[x]
-        return f' {letter}{y+1}({x}, {y})'
 
 def print_board(square):
     for y in reversed(range(8)):
@@ -24,4 +12,4 @@ def print_board(square):
             print(square(x, y), end='')
         print()
 
-print_board(nw_se_diagonal_str)
+print("OK")
